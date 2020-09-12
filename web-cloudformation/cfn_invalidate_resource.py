10f4db344cea21046d4e1d7528ccb74b683d30e6
Jim Thario <jtthario@amazon.com>
2020-09-12 16:16:54 -0700

    Update custom resource for #134

--- web-cloudformation/cfn_invalidate_resource.py
+++ web-cloudformation/cfn_invalidate_resource.py
@@ -1,7 +1,13 @@
-from crhelper import CfnResource
+"""
+This custom resource is responsible for issuing an
+invalidation to CloudFront after the web content is changed.
+"""
+
 import logging
 import time
+
 import boto3
+from crhelper import CfnResource
 
 logger = logging.getLogger(__name__)
 helper = CfnResource()
@@ -9,31 +15,28 @@ helper = CfnResource()
 
 @helper.create
 @helper.update
-def create_update(event, context):
-    logger.info(event)
-    try:
-        distribution_id = event['ResourceProperties']['DistributionId']
-        cloudfront = boto3.client("cloudfront")
-        response = cloudfront.create_invalidation(
-            DistributionId=distribution_id,
-            InvalidationBatch={
-                'Paths': {
-                    'Quantity': 1,
-                    'Items': [
-                        '/*',
-                    ]
-                },
-                'CallerReference': str(int(time.time()))
-            })
-        logger.info(response)
-    except Exception as exception:
-        logger.error(exception)
-
-
-@helper.delete
-def delete(event, context):
-    logger.info(event)
+def create_update(event, _):
+    """
+    This function issues an invalidation command to CloudFront.
+    """
+    distribution_id = event['ResourceProperties']['DistributionId']
+    cloudfront = boto3.client("cloudfront")
+    response = cloudfront.create_invalidation(DistributionId=distribution_id,
+                                              InvalidationBatch={
+                                                  'Paths': {
+                                                      'Quantity': 1,
+                                                      'Items': [
+                                                          '/*',
+                                                      ]
+                                                  },
+                                                  'CallerReference':
+                                                  str(int(time.time()))
+                                              })
+    logger.info(response)
 
 
 def handler(event, context):
+    """
+    This is the Lambda custom resource entry point.
+    """
     helper(event, context)
