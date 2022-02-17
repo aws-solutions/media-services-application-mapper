f3b876cd6f0d8db2be6c0a998ffdce52aeb2b102
Jim Thario <jtthario@amazon.com>
2022-02-17 13:35:50 -0800

    Refinements for pipeline build

---
+++ deployment/build-open-source-dist.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This assumes all of the OS-level configuration has been completed and git repo has already been cloned
+#
+# This script should be run from the repo's deployment directory
+# cd deployment
+# ./build-open-source-dist.sh solution-name
+#
+# Paramenters:
+#  - solution-name: name of the solution for consistency
+
+# Check to see if input has been provided:
+if [ -z "$1" ]; then
+    echo "Please provide the trademark approved solution name for the open source package."
+    echo "For example: ./build-open-source-dist.sh trademarked-solution-name"
+    exit 1
+fi
+
+# create the folder for compatibility
+mkdir -p open-source
+
+exit 0
