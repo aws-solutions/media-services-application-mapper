6d0bf236a83236ea86583c2b9f0d3fc7fe4ada3a
Jim Thario <jtthario@amazon.com>
2022-02-17 10:06:48 -0800

    Refine comparison for venv check

--- deployment/venv_check.py
+++ deployment/venv_check.py
@@ -5,7 +5,7 @@ This program returns 0 if the current environment is a virtual environment.
 import sys
 
 # compare the python prefixes, same == not venv
-in_venv = (getattr(sys, "base_prefix", None) or getattr(
+IN_VENV = (getattr(sys, "base_prefix", None) or getattr(
     sys, "real_prefix", None) or sys.prefix) != sys.prefix
 # return success (0) if in a venv
-sys.exit(in_venv == False)
+sys.exit(IN_VENV is False)
