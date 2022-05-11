80ed14501b73ba4f39b3088e6c98122db32ca4b0
Jim Thario <jtthario@amazon.com>
2022-05-11 14:11:16 -0700

    Unit tests and code coverage reporting updates

--- source/msam/test/run_unit_tests.py
+++ source/msam/test/run_unit_tests.py
@@ -6,9 +6,10 @@ python -m test.run_unit_tests
 import unittest
 
 # pylint: disable=W0611,E0611
-from test.test_cache import TestCache
-from test.test_channels import TestChannels
-from test.test_cloudwatch import TestCloudWatch
+from test.test_cache import *
+from test.test_channels import *
+from test.test_cloudwatch import *
+from test.test_connections import *
 
 if __name__ == '__main__':
     unittest.main(verbosity=3)
