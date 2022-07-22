5c4b94511defe0e04efe84dc600778dab1409e6c
joan <morjoan@amazon.com>
2022-07-21 18:48:14 -0700

    debugging unittest failure

--- source/events/test/test_cloudwatch_alarm.py
+++ source/events/test/test_cloudwatch_alarm.py
@@ -4,6 +4,7 @@ This module is provides unit tests for the cloudwatch_alarm.py module.
 
 # pylint: disable=C0415,W0201
 
+import os
 import unittest
 from unittest.mock import patch, MagicMock
 from botocore.exceptions import ClientError
@@ -11,20 +12,22 @@ from botocore.exceptions import ClientError
 ARN = "arn:msam:user-defined-node:global:111122223333:10AA8D40-2B6F-44FA-AA67-6B909F8B1DB9"
 CLIENT_ERROR = ClientError({"Error": {"Code": "400", "Message": "SomeClientError"}}, "ClientError")
 
+os.environ["SOLUTION_ID"] = "SO0166"
+os.environ["ALARMS_TABLE_NAME"] = "alarms_table"
+os.environ["EVENTS_TABLE_REGION"] = "us-east-1"
+
 @patch('boto3.client')
 @patch('boto3.resource')
-@patch('os.environ')
 class TestCloudWatchAlarm(unittest.TestCase):
     """
     This class extends TestCase with testing functions
     """
-    def test_subscribers_to_alarm(self, patched_env, patched_resource,
+    def test_subscribers_to_alarm(self, patched_resource,
                                      patched_client):
         """
         Test the subscribers_to_alarm function
         """
         import cloudwatch_alarm
-        del patched_env['PATH']
         with patch.object(cloudwatch_alarm.ALARMS_TABLE, 'query', side_effect=[{"Items":[{"ResourceArn": ARN}], "LastEvaluatedKey": "token"},
             {"Items":[{"ResourceArn": ARN}]}]):
             cloudwatch_alarm.subscribers_to_alarm("RegionAlarmName")
@@ -34,13 +37,12 @@ class TestCloudWatchAlarm(unittest.TestCase):
             self.assertRaises(ClientError)
 
 
-    def test_lambda_handler(self, patched_env, patched_resource,
+    def test_lambda_handler(self, patched_resource,
                                      patched_client):
         """
         Test the lambda_handler function
         """
         import cloudwatch_alarm
-        del patched_env['PATH']
         mocked_event = {"region": "us-east-1", "detail": {"alarmName": "alarmName"}}
         patched_resource.return_value.Alarm.return_value.alarm_value = "set"
         with patch.object(cloudwatch_alarm, 'subscribers_to_alarm', return_value=[ARN]):
